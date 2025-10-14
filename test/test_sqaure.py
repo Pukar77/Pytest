60f31d446c6b8d7be09fa9eaf63f413a4d41f34c
Pukar77 <pukarrimal11@gmail.com>
2025-10-14 14:14:34 +0545

    Add parameterized test for Square area calculation

--- test/test_sqaure.py
+++ test/test_sqaure.py
@@ -1,3 +1,10 @@
 import pytest
 from source import classbasetest
 
+@pytest.mark.parametrize('actual_area, expected_area', [(2,4), (3,9), (4,16)])
+def test_area(actual_area, expected_area):
+    sq = classbasetest.Square(actual_area)
+    assert sq.area() == expected_area
+
+
+
